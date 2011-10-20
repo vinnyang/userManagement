78448d535d74fe9d7792e51f63761e0d133f900a
Vincent Yang <yusyang686@gmail.com>
2011-10-20 15:15:31 -0400

    fixedRelationship

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,3 @@
 class User < ActiveRecord::Base
-  has_many :roles
+  belongs_to :role
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20111020175233) do
+ActiveRecord::Schema.define(:version => 20111020190305) do
 
   create_table "roles", :force => true do |t|
     t.datetime "created_at"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(:version => 20111020175233) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "Password"
+    t.integer  "role_id"
   end
 
 end
