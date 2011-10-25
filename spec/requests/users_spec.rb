e1ae6f671ccd840e5e9dcc303a2e5f7bac14efb5
Vincent Yang <yusyang686@gmail.com>
2011-10-25 01:43:36 -0400

    commaDelimitedRolesInTheViewDoneMoreTestsOnTheWay

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,8 +1,8 @@
 require 'spec_helper'
 
-describe "Users" do
+describe "UserRoutes" do
   describe "GET /users" do
-    it "works! (now write some real specs)" do
+    it "should have a path for the 'Index' action" do
       # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
       get users_path
       response.status.should be(200)
@@ -10,8 +10,21 @@ describe "Users" do
   end
   
   describe "GET 'new'" do
-    it "should be successful" do
-      get 'new'
+    it "should be the correct path for 'New' action" do
+      get new_user_path
+      response.should be_success
+    end
+  end
+  
+  describe "GET 'edit'" do
+    it "should be the correct path for 'Edit' action" do
+      get edit_user_path
+      response.should be_success
+    end
+  end
+  describe "GET 'user'" do
+    it "should be the correct path for 'Show' action" do
+      get user_path
       response.should be_success
     end
   end
